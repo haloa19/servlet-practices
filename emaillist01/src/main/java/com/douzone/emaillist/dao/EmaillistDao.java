0991b27565f80064f2ac7fcc71f0e63bd58c40fd
haloa19 <hj199514@gmail.com>
2020-02-18 14:04:01 +0900

    패키징이름 변경

--- emaillist01/src/main/java/com/douzone/emaillist/dao/EmaillistDao.java
+++ emaillist01/src/main/java/com/douzone/emaillist/dao/EmaillistDao.java
@@ -121,7 +121,7 @@ public class EmaillistDao {
 			// 1. jdbc driver(myDriver) 로딩
 			Class.forName("org.mariadb.jdbc.Driver");			
 			// 2. 연결하기
-			String url = "jdbc:mysql://127.0.0.1:3307/webdb";
+			String url = "jdbc:mysql://192.168.1.113:3307/webdb";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		}  catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패" + e);
